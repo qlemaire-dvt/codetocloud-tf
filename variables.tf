a5bf1706c013e00dc8471eb5ab88ae05522a8d9e
qlemaire-dvt <quentin.lemaire56@gmail.com>
2021-03-09 11:15:03 +0100

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+}
+
+variable "trigram" {
+    description = "This prefix consists of 3 letters, 1st one of your first name, then the 2 first letters of your last name"
+}
+
